68fc18bce5664caf4f85be16b2c98d44a9e2baf3
EmilianoGarciaLopez <emilianogarcialopez26@gmail.com>
2021-11-14 16:04:41 -0500

    started adding sanity implementation of grid

--- sanity-client.js
+++ sanity-client.js
@@ -2,8 +2,8 @@
 
 const sanityClient = require('@sanity/client')
 const client = sanityClient({
-  projectId: 'your-project-id',
-  dataset: 'bikeshop',
-  apiVersion: '2021-03-25', // use current UTC date - see "specifying API version"!
+  projectId: '2hsh4d2g',
+  dataset: 'production',
+  apiVersion: '2021-11-14', // use current UTC date - see "specifying API version"!
   useCdn: true, // `false` if you want to ensure fresh data
 })
