2d7223ad48dc39f74fbca6344c3b0797535cd96b
EmilianoGarciaLopez <emilianogarcialopez26@gmail.com>
2021-11-17 11:31:10 -0500

    added sanity.io urlFor & configured client

--- grid.module.js
+++ grid.module.js
@@ -1,10 +1,20 @@
-import client from '../sanity-client';
+import client from './sanity-client.js';
+import imageUrlBuilder from '@sanity/image-url'
 
-const query = ``;
-const params = {};
+const query = `*[_type == "Hearts"] {
+  name,
+  image,
+  bio,
+}`;
 
-client.fetch(query, params).then((heart) => {
-    picture.forEach((heart) => {
-      console.log(`author:${heart.name} \n bio:${heart.bio}`)
+const builder = imageUrlBuilder(client)
+
+function urlFor(source) {
+  return builder.image(source)
+}
+
+client.fetch(query).then((heart) => {
+    heart.forEach((heart) => {
+      console.log(`author:${heart.name} \n bio:${heart.bio} \n image:${urlFor(heart.image).url()}`)
     })
   })
--- sanity-client.js
+++ sanity-client.js
@@ -1,9 +1,12 @@
 // Sanity client 
+import sanityClient from '@sanity/client';
 
-const sanityClient = require('@sanity/client')
+
 const client = sanityClient({
   projectId: '2hsh4d2g',
   dataset: 'production',
   apiVersion: '2021-11-14', // use current UTC date - see "specifying API version"!
   useCdn: true, // `false` if you want to ensure fresh data
 })
+
+export default client
