13d5894fcf72a8fcc862dac029ea2983d7d3ed67
EmilianoGarciaLopez <emilianogarcialopez26@gmail.com>
2021-11-18 16:51:02 -0500

    finished grid

--- sanity-client.js
+++ sanity-client.js
@@ -4,8 +4,8 @@ import sanityClient from "@sanity/client";
 const client = sanityClient({
   projectId: "2hsh4d2g",
   dataset: "production",
-  apiVersion: "2021-11-14", // use current UTC date - see "specifying API version"!
-  useCdn: false, // `false` if you want to ensure fresh data
+  apiVersion: "2021-11-18", // use current UTC date - see "specifying API version"!
+  useCdn: true, // `false` if you want to ensure fresh data
 });
 
 export default client;
